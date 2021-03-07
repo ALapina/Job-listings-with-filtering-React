bc9548e5828d81844542367d9ea44a6ed921df46
ALapina <larisa_42@mail.ru>
2021-03-07 12:11:12 +0200

    update tailwind config

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,5 +1,5 @@
 module.exports = {
-  purge: ["./public/*.html", "./src/*.js"],
+  purge: ["./public/*.html", "./src/**/*.js"],
   darkMode: false, // or 'media' or 'class'
   theme: {
     extend: {
